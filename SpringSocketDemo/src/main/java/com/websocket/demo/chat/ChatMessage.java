c865144436910ffb407ee3a4b35a69dabdda12ef
Natan <Th3than@gmail.com>
2024-02-18 18:54:40 +0700

    Add ConnectCount

--- SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
@@ -11,4 +11,13 @@ public class ChatMessage {
     private String timestamp;
     private String sender;
     private MessageType type;
+    public static int CC;
+    public int increaseCC(){
+        CC += 1;
+        return CC;
+    }
+    public int decreaseCC(){
+        CC -= 1;
+        return CC;
+    }
 }
