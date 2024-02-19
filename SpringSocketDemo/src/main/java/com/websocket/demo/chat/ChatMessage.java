5adb76fc6409729aed3cdddec41529786ad3fa6e
Natan <Th3than@gmail.com>
2024-02-19 13:10:02 +0700

    Add Backend and Frontend

--- SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
@@ -3,20 +3,23 @@ package com.websocket.demo.chat;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Builder
+@Setter
 public class ChatMessage {
     private String content;
     private String timestamp;
     private String sender;
     private MessageType type;
+    private int chatCC;
     public static int CC;
-    public int increaseCC(){
+    public static int increaseCC(){
         CC += 1;
         return CC;
     }
-    public int decreaseCC(){
+    public static int decreaseCC(){
         CC -= 1;
         return CC;
     }
--- SpringSocketDemo/src/main/java/com/websocket/demo/chat/chatController.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/chat/chatController.java
@@ -18,6 +18,7 @@ public class chatController {
     @SendTo("/topic/public")
     public ChatMessage addUser(ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        chatMessage.setChatCC(ChatMessage.increaseCC());
         return chatMessage;
     }
 }
